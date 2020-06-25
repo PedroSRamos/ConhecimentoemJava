96af844c02ef8630117b0bd1c449fb78c77bf7da
Pedro Henrique de Souza Ramos <36521649+PedroSRamos@users.noreply.github.com>
2020-06-25 20:22:12 -0300

    Add files via upload

---
+++ 2_questao.java
@@ -0,0 +1,11 @@
+package br.edu.univas.main;
+
+import java.util.Scanner;
+
+public class Questao2 {
+    public static void main(string[] args) {
+        Scanner leitura = new Scanner (system.in);
+        int numeros = 
+        
+    }
+}
---
+++ 3_questao.java
@@ -0,0 +1,11 @@
+package br.edu.univas.main;
+
+import java.util.Scanner;
+
+public class Questao3 {
+    public static void main(string[] args) {
+        Scanner leitura = new Scanner (system.in);
+        int numeros = 
+        
+    }
+}
